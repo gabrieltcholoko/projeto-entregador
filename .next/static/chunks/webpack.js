d89d458bfb91f3ef8e5f7858c322ed49c989f863
GABRIEL COSTA PEREIRA <103195@campus.sertao.ifrs.edu.br>
2022-06-29 10:03:54 -0300

    retirada do .next por que ele é criado com o npm run dev

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b3fc6ed5719edd2"; }
+/******/ 		__webpack_require__.h = function() { return "5da874a55dec0303"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
